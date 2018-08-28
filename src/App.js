726d6a67e013d123de6fc7461e5a02fb19996730
Jeremy Rojas <jeremytri@Jeremys-MacBook-Air.local>
2018-08-28 02:20:24 -0400

    cleanup, prep for github page deployment, improved responsiveness

--- src/App.js
+++ src/App.js
@@ -8,32 +8,41 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar>
-          <Nav pullRight className="navbar-buttons">
-            <NavItem eventKey={1} href="#">
-              About
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Projects
-            </NavItem>
-            <NavItem eventKey={3} href="#">
-              Contact
-            </NavItem>
-          </Nav>
+
+{/*        <Navbar collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Toggle/>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight className="navbar-buttons">
+              <NavItem eventKey={1} href="#">
+                About
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Projects
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                Contact
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
+*/}
         <Jumbotron className="jumbo">
           <Grid>
             <Image width={200} className="img-fluid" src={profilePic} circle />
           </Grid>
-          <h1 className="main-header-font">Hi, I'm Jeremy Rojas!</h1>
-          <p className="main-header-subtitle-font">
-            I'm a software engineer and I love to create.
-          </p>
+          <div className="jumbo-text">
+            <h1 className="main-header-font">Hi, I'm Jeremy Rojas!</h1>
+            <p className="main-header-subtitle-font">
+              I'm a software engineer and I love to create with code.
+            </p>
+          </div>
           <div>
+            <SocialIcon url="https://github.com/jeremyrojas" className="social-icon-left"/>
             <SocialIcon url="https://www.linkedin.com/in/jeremytrojas/jeremy_tri"/>
-            <SocialIcon url="https://github.com/jeremytrojas"/>
             <SocialIcon url="https://twitter.com/jeremy_tri"/>
-            <SocialIcon url="https://www.instagram.com/jeremy_tri/"/>                        
+            <SocialIcon url="https://www.instagram.com/jeremy_tri/"/>
           </div>
         </Jumbotron>
       </div>
